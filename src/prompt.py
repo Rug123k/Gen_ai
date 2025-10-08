f89199c08a0792c0fd1713794e62672d431755f2
Rug123k <rugvedkulkarni0019@gmail.com>
2025-10-08 13:50:35 +0530

    only avilble on local system in next we upload for public use

---
+++ src/prompt.py
@@ -0,0 +1,8 @@
+system_prompt=(
+    "You are a helpful AI assistant. Use the following pieces of context to answer the question at the end. "
+    "the question if you don't know the answer, just say that you don't know, don't try to make up an answer. "
+    "use three sentences maximum and keep it very concise. "
+    "\n \n"
+    "{context}"
+
+)
